f314ebfc09505ed21a890850d0e1e0bd930607b9
Dave J Schoepel <dave@theschoepels.com>
2023-06-13 19:47:07 -0500

    debugging directory structure

--- src/serverV1.js
+++ src/serverV1.js
@@ -43,6 +43,7 @@ app.use("/api/v1/file", v1FileRouter);
 
 // default route
 app.get("/", (req, res) => {
+  console.log("Dirname = ", __dirname);
   // res.sendFile(path.join(__dirname + "/v1/public/index.html"));
   res.json({
     message: "Welcome to the PDFAssetExtractor API.",
