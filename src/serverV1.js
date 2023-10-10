b5d0bee8e9283da5d3c1e57ae9a3dd2ed31ef36f
Dave J Schoepel <dave@theschoepels.com>
2023-10-09 19:13:54 -0500

    testing limit for file size

--- src/serverV1.js
+++ src/serverV1.js
@@ -14,9 +14,10 @@ global.__basedir = __dirname;
 
 app.use(helmet());
 app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const welcomeMsg = { title: "Welcome to the pdf table extractor application." };
